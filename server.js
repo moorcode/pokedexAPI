bf5279a6c6c1213cb0c1d4e3fbf2c36f6a50b6bc
Christina Poole <codenamedchris@gmail.com>
2021-01-09 19:20:43 -0500

    fix dependencies

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 require('dotenv').config(); // do this asap
 const express = require('express');
 const morgan = require('morgan');
-const helmet = require('helmet');
 const cors = require('cors');
 const POKEDEX = require('./pokedex.json');
 
@@ -11,14 +10,15 @@ const app = express();
 
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganSetting));
-app.use(helmet());
 app.use(cors());
+
 app.use(function validateBearerToken(req, res, next) {
-  // const apiToken = process.env.API_TOKEN;
-  // const authToken = req.get('Authorization');
-  // if (!authToken || authToken.split(' ')[1] !== apiToken) {
-  //   return res.status(401).json({ error: 'Unauthorized request' });
-  // }
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
   next();
 });
 
@@ -26,7 +26,6 @@ const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `F
 
 // middleware written and used as callback
 // function in request handler
-// res.json sends data as json
 function handleGetTypes(req, res) {
   res.json(validTypes);
 }
@@ -38,6 +37,7 @@ function handleGetPokemon(req, res) {
   let response = POKEDEX.pokemon;
 
   if (req.query.name) {
+    // filter pokemon by name if name query param is present
     response = response.filter(pokemon =>
       // case insensitive searching
       pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
